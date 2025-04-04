d5859731af8564cf03b52f4bf65c3385d4c0ea43
GarnierBaptiste <garnier.baptiste92@gmail.com>
2025-04-04 14:55:29 +0200

    Un_pas MT

--- machine_turing.py
+++ machine_turing.py
@@ -1,9 +1,10 @@
 
 class Machine_Turing():
-    def __init__(self,etats):
-        self.ruban=[]
-        self.alphabet=set(0,1,'_')
-        self.etat=etats
+    def __init__(self,etats,mot):
+        self.ruban = [elem for elem in mot]
+        self.alphabet= set()
+        self.etat = set(etats)
+        self.etat_actuel = etats[0]
         self.curseur=0
 
 class Configuration():
@@ -12,23 +13,41 @@ class Configuration():
         
 def lecture(fichier):
     with open(fichier,'r') as f:
-        etats=set()
-        configuration=[]
-        lignes=f.readlines()
-        mot=lignes[0][0].replace('\n','')
-        etats.add(lignes[1][0].replace('\n',''))
-        etats.add(lignes[2][0].replace('\n',''))
-        for ligne in lignes[2:]:
-            ligne=ligne.split(',')
-            etats.add(ligne[0])
-            etats.add(ligne[2])
-            configuration.append((ligne[0],ligne[1],ligne[2],ligne[3],ligne[4].replace('\n','')))
+        etats = []
+        configuration = {}
+        lignes = f.readlines()
+        mot = lignes[0].replace('\n','')
+        etats.append(lignes[1].replace('\n',''))
+        etats.append(lignes[2].replace('\n',''))
+        for ligne in lignes[3:]:
+            ligne = ligne.split(',')
+            etats.append(ligne[0])
+            etats.append(ligne[2])
+            configuration[(ligne[0],ligne[1])] = (ligne[2],ligne[3],ligne[4].replace('\n',''))
         return mot,configuration,etats
         
-        
-lecture('example.txt')
-        
-        
 
+def un_pas(mt : Machine_Turing, config : Configuration):
+    etat = (mt.etat_actuel,mt.ruban[mt.curseur])
+    mt.ruban[mt.curseur] = config.configuration[etat][1]
+    match config.configuration[etat][2]:
+        case '<':
+            if mt.curseur > 0:
+                mt.curseur -= 1
+            else:
+                mt.ruban = ['_'] + mt.ruban
+        case '>':
+            mt.curseur += 1
+            if mt.curseur >= len(mt.ruban)-1:
+                mt.ruban.appen('_')
+        case '_':
+            pass
+    mt.etat_actuel = config.configuration[etat][0]
 
-
+if __name__ == "__main__":
+    lec = lecture('example.txt')
+    mt = Machine_Turing(lec[2],lec[0])
+    config = Configuration(lec[1])
+    print(un_pas(mt,config))
+    print(mt.ruban)
+    print(mt.etat_actuel)
