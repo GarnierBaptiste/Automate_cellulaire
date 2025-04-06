c90629032db934809727978acd1d2d9822972907
uvsq22105679 <hajar.cherqi@ens.uvsq.fr>
2025-04-07 00:25:01 +0200

    ajout page d'accueil

--- Fichier_Python.py/Visualisation.py
+++ Fichier_Python.py/Visualisation.py
@@ -3,6 +3,8 @@ from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.button import Button
 from kivy.uix.textinput import TextInput
 from kivy.uix.label import Label
+from kivy.uix.screenmanager import ScreenManager, Screen
+
 
 class Visualisation(BoxLayout):
     def __init__(self, mot_entree):
@@ -16,6 +18,7 @@ class Visualisation(BoxLayout):
         lab2 = Label(text='Etape numéro : 0')
         self.add_widget(lab1)
         self.add_widget(lab2)
+        
 
 class Ecriture(BoxLayout):
     def __init__(self):
@@ -36,7 +39,15 @@ class Mode(BoxLayout):
         self.orientation = 'vertical'
         self.add_widget(Button(text='Mode 1',size_hint=(0.5, 0.5)))
         self.add_widget(Button(text='Mode 2',size_hint=(0.5, 0.5)))
+        bn_retour=Button(text='Retour',size_hint=(0.5, 0.5))
+        bn_retour.bind(on_release=self.retour)
+        self.add_widget(bn_retour)
 
+    def retour(self,instance):
+        App.get_running_app().root.current = 'accueil'
+
+        
+        
 class Choix(BoxLayout):
     def __init__(self):
         super(Choix, self).__init__()
@@ -55,15 +66,49 @@ class Affichage(BoxLayout):
         choix = Choix()
         self.add_widget(choix)
 
-class AutomateApp(App):
-    def build(self):
+class AutomateApp(Screen):
+    def __init__(self,name):
+        super(AutomateApp, self).__init__()
+        self.name=name
         layout = BoxLayout(orientation='vertical')
         bn1 = Button(text='Button 1')
         bn2 = Button(text='Button 2')
+        bn3=Button(text='Button 3')
         layout.add_widget(bn1)
         layout.add_widget(bn2)
-        return Affichage()
+        layout.add_widget(bn3)
+        self.add_widget(Affichage())
+        
+        
+        
 
-
+class EcranDaccueil(Screen):
+    def __init__(self,name):
+        super(EcranDaccueil, self).__init__()
+        self.name=name
+        layout=BoxLayout(orientation='vertical', spacing=20, padding=40)
+        lab1=Label(text='Bienvenue sur votre simulateur de Machine de turing et d Automate Cellulaire',font_size=30)
+        btn1=Button(text='Macine de turing',size_hint=(0.2,0.2),pos_hint={'center_x': 0.5, 'center_y': 0.5})
+        btn2=Button(text='Automate Cellulaire',size_hint=(0.2,0.2),pos_hint={'center_x': 0.5, 'center_y': 0.5})
+        btn1.bind(on_release=self.new_page)
+        btn2.bind(on_release=self.new_page)
+        layout.add_widget(lab1)
+        layout.add_widget(btn1)
+        layout.add_widget(btn2)
+        self.add_widget(layout)
+        
+    def new_page(self,instance):
+        self.manager.current='simulation'
+    
+class GestionEcrans(App):
+    def build(self):
+        sm = ScreenManager()
+        accueil = EcranDaccueil('accueil')
+        simulation = AutomateApp('simulation')
+        
+        sm.add_widget(EcranDaccueil('accueil'))
+        sm.add_widget(AutomateApp('simulation'))
+        return sm
+        
 if __name__ == '__main__':
-    AutomateApp().run()
+    GestionEcrans().run()
